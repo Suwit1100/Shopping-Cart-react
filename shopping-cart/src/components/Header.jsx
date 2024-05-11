7a37789f332ef0085c09e5f21af263ebd1fcf5fb
สุวิทย์ ลีเจ้ยวาระ <139098129+Suwit1100@users.noreply.github.com>
2024-05-11 13:44:16 +0700

    ติดตั้ง

---
+++ shopping-cart/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../css/Header.css"
+
+function Header() {
+    return (
+        <>
+            <header >
+                <span>Shopping Cart </span>
+                <span>สินค้าในตระกร้า : 0</span>
+            </header>
+        </>
+    )
+}
+
+export default Header

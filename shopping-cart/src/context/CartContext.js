e2951d28a0d88f4a96d1976a52dc3995e16f631c
สุวิทย์ ลีเจ้ยวาระ <139098129+Suwit1100@users.noreply.github.com>
2024-05-11 17:16:01 +0700

    สร้าง povider ตัวกระจายข้อมูล

--- shopping-cart/src/context/CartContext.js
+++ shopping-cart/src/context/CartContext.js
@@ -1,5 +1,6 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 import products from "../data/product";
+import CartReducer from "../reducer/CartReducer";
 // สร้าง Context เปล่าๆ
 const CartContext = createContext()
 const initState = {
@@ -7,6 +8,16 @@ const initState = {
     amouth: 0,
     total: 0
 };
+
+// สร้าง Provider ไว้กระจายข้อมูล
+export const Provider = ({ children }) => {
+    const [state, dispatch] = useReducer(CartReducer, initState)
+    return (
+        <CartContext.Provider value={{ ...state }}>
+            {children}
+        </CartContext.Provider>
+    )
+}
 
 //นำ Context ออกไปใช้
 export const useCart = () => {
--- shopping-cart/src/reducer/CartReducer.js
+++ shopping-cart/src/reducer/CartReducer.js
@@ -1,6 +1,5 @@
 // สร้าง Reducer
 const CartReducer = (state, action) => {
     //กระบวนการจัดการ state ผ่าน action
-    
 }
 export default CartReducer;

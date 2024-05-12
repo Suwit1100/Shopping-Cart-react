32c24c5dd44bce0ed46d51824916b37c68bb950e
สุวิทย์ ลีเจ้ยวาระ <139098129+Suwit1100@users.noreply.github.com>
2024-05-12 13:34:17 +0700

    style item

--- shopping-cart/src/components/Item.jsx
+++ shopping-cart/src/components/Item.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ImageImport from "../../public/assets/product1.png"
+import "../css/Item.css"
 function Item(props) {
     const { id, name, price, image, quantity } = props;
     return (
@@ -10,6 +10,13 @@ function Item(props) {
                     <p className="name">ชื่อสินค้า : {name}</p>
                     <p className="price">ราคา : {price} บาท</p>
                 </div>
+                <div className="quantity">
+                    <button>+</button>
+                    <input type="text" value={quantity} />
+                    <button>-</button>
+                </div>
+                <div className="total-price">{quantity * price}</div>
+                <button>ลบสิ้นค้า</button>
             </div>
         </div>
     )
